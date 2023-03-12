e7d5715cfdc2fcf32213a8773aab4b7ed986bb7d
ivonastevanovicc <124252301+ivonastevanovicc@users.noreply.github.com>
2023-03-12 19:43:02 +0100

    v1.0

---
+++ EsDnevnik/EsDnevnik/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EsDnevnik
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void odeljenjaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Odeljenje odeljenje = new Odeljenje();
+            odeljenje.Show();
+        }
+
+        private void osobaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Osoba osoba = new Osoba();
+            osoba.Show();
+        }
+    }
+}
